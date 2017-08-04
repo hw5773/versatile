46f26a42fe19650d97a0075f8dcf7abdbffab9ab
Hyunwoo Lee <hwlee2014@mmlab.snu.ac.kr>
2017-08-04 20:37:03 +0900

    update flex

--- wireshark/packet-flex.c
+++ wireshark/packet-flex.c
@@ -1,119 +1,160 @@
 #include "config.h"
-#include <epan/packet.h>
-#include <epan/exceptions.h>
-#include <epan/ipproto.h>
-#include <epan/addr_resolv.h>
 #include "packet-flex.h"
-#include "packet-tcp.h"
-#include <epan/prefs.h>
-#include <epan/strutil.h>
-#include <epan/expert.h>
-#include <epan/afn.h>
-#include <epan/tap.h>
-#include <epan/stats_tree.h>
-
-void proto_register_flex(void);
-void proto_reg_handoff_flex(void);
+#include <epan/packet.h>
 
 static int proto_flex = -1;
+static int hf_flex_version = -1;
+static int hf_flex_packet_type = -1;
+static int hf_flex_hash_type = -1;
+static int hf_flex_hop_limit = -1;
+static int hf_flex_header_length = -1;
+static int hf_flex_checksum = -1;
+static int hf_flex_packet_id = -1;
+static int hf_flex_ptc = -1;
+static int hf_flex_df = -1;
+static int hf_flex_mf = -1;
+static int hf_flex_frag_offset = -1;
+static int hf_flex_src_id = -1;
+static int hf_flex_dst_id = -1;
+static int hf_flex_packet_length = -1;
+static int hf_flex_sequence = -1;
+static int hf_flex_acknowledge = -1;
 
-static gint ett_flex = -1;
+static gint ett_foo = -1;
 
-static expert_field ei_flex_wrong_value = EI_INIT;
+static const value_string version[] = {
+	{ FLEX_1_0, "Flex v1.0"}
+};
 
-/* Test Codes */
-#define TEST_QUERY	0
-#define TEST_ANSWER	1
-#define TEST_INFO	2
-
-static const value_string test_code_vals[] = {
-	{ TEST_QUERY, 	"Test query"	},
-	{ TEST_ANSWER, 	"Test answer"	},
-	{ TEST_INFO, 	"Test info"		},
-	{ 0,			NULL			} };
-
-static int flex_tap = -1;
-
-static int hf_flex_test = -1; 					// Header Field (hf)
-static gint ett_flex = -1;						// Ethereal Tree Type (ett)
-static expert_field ei_flex_wrong_value = -1;	// Error Indication (ei)
-static dissector_handle_t flex_handle = -1;		// Dessector
-static range_t *global_flex_port_range;			// Range
-
-/* Ports used for Flex. */
-#define DEFAULT_FLEX_PORT_RANGE		"3333"
-
-/* Offsets of fields within an Flex packet. */
-#define FLEX_TEST					0
+static const value_string packet_type[] = {
+	{ JOIN, "Join" },
+	{ STATUS, "Status" },
+	{ LEAVE, "Leave" },
+	{ REGISTER, "Register" },
+	{ UPDATE, "Update" },
+	{ QUERY, "Query" },
+	{ REQUEST, "Request" },
+	{ JOIN_ACK, "Join ACK" },
+	{ STATUS_ACK, "Status ACK" },
+	{ LEAVE_ACK, "Leave ACK" },
+	{ REGISTER_ACK, "Register ACK" },
+	{ UPDATE_ACK, "Update ACK" },
+	{ REPLY, "Reply" },
+	{ RESPONSE, "Response" },
+	{ INTEREST, "Interest" },
+	{ DATA, "Data" },
+	{ DATA_ACK, "Data ACK" }
+};
 
 static int
-dissect_flex(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void * data)
+dissect_foo(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree _U_, void *data _U_)
 {
-	guint16		flex_test;
+	gint offset = 0;
+	gint8 packet_type = tvb_get_guint8(tvb, 0);
+
 	col_set_str(pinfo->cinfo, COL_PROTOCOL, "FLEX");
 	col_clear(pinfo->cinfo, COL_INFO);
+	col_add_fstr(pinfo->cinfo, COL_INFO, "Type %s",
+			val_to_str(packet_type, packettypenames, "Unknown (0x%02x)"));
 
-	/* Test Value */
-	flex_test = tvb_get_ntohs(tvb, FLEX_TEST);
+	proto_item *ti = proto_tree_add_item(tree, proto_foo, tvb, 0, -1, ENC_NA);
+	proto_item_append_text(ti, ", Type %s",
+			val_to_str(packet_type, packettypenames, "UNknown (0x%02x)"));
+	proto_tree *foo_tree = proto_item_add_subtree(ti, ett_foo);
+	proto_tree_add_item(foo_tree, hf_foo_pdu_type, tvb, offset, 1, ENC_BIG_ENDIAN);
+	offset += 1;
+	proto_tree_add_item(foo_tree, hf_foo_flags, tvb, offset, 1, ENC_BIG_ENDIAN);
+	proto_tree_add_item(foo_tree, hf_foo_startflag, tvb, offset, 1, ENC_BIG_ENDIAN);
+	proto_tree_add_item(foo_tree, hf_foo_endflag, tvb, offset, 1, ENC_BIG_ENDIAN);
+	proto_tree_add_item(foo_tree, hf_foo_priorityflag, tvb, offset, 1, ENC_BIG_ENDIAN);
+	offset += 1;
+	proto_tree_add_item(foo_tree, hf_foo_sequenceno, tvb, offset, 2, ENC_BIG_ENDIAN);
+	offset += 2;
+	proto_tree_add_item(foo_tree, hf_foo_initialip, tvb, offset, 4, ENC_BIG_ENDIAN);
+	offset += 4; 
+
+	return tvb_captured_length(tvb);
 }
 
-void
-proto_register_flex(void)
+void 
+proto_register_foo(void)
 {
 	static hf_register_info hf[] = {
-		{ &hf_flex_test,
-			{ "Test", "flex.test",
-				FT_UINT16, BASE_HEX, VALS(test_code_vals), 0x0,
-				"Test code", HFILL }}};
-
-	static ei_register_info ei[] = {
-		{ &ei_flex_wrong_value, { "flex.wrong_value", PI_MALFORMED, PI_ERROR, "Wrong value", EXPFILL}},
+		{ &hf_flex_version,
+			{ "Version", "flex.version",
+			FT_UINT8, BASE_HEX,
+			VALS(version), 0x0,
+			NULL, HFILL }
+		},
+		{ &hf_flex_packet_type
+			{ "Packet Type", "flex.packet_type",
+			FT_UINT8, BASE_HEX,
+			VALS(packet_type), 0x0,
+			NULL, HFILL}
+		},
+		{ &hf_flex_hash_type,
+			{ "Hash Type", "flex.hash_type",
+			FT_UINT8, BASE_HEX,
+			NULL, 0x0,
+			NULL, HFILL }
+		},
+		{ &hf_flex_hop_limit,
+			{ "Hop limit", "flex.hop_limit",
+			FT_UINT8, BASE_DEC,
+			NULL, 0x0,
+			NULL, HFILL }
+		},
+		{ &hf_flex_header_length,
+			{ "Header Length", "flex.header_length",
+			FT_UINT8, BASE_DEC,
+			NULL, 0x0,
+			NULL, HFILL }
+		},
+		{ &hf_flex_checksum,
+			{ "Checksum", "flex.checksum",
+			FT_UINT8, BASE_HEX,
+			NULL, 0x0,
+			NULL, HFILL }
+		},
+		{ &hf_flex_packet_id,
+			{ "Packet ID", "flex.packet_id",
+			FT_UINT16, BASE_HEX,
+			NULL, 0x0,
+			NULL, HFILL }
+		},
+		{ &hf_flex_ptc,
+			{ "PTC", "flex.ptc",
+			FT_BOOLEAN, 8,
+			NULL, FLEX_PTC,
+			NULL, HFILL }
+		}
+		/* Need to add fields for
+		 * hf_flex_ptc, hf_flex_df, hf_flex_mf, hf_flex_frag_offset, 
+		 * hf_flex_src_id, hf_flex_dst_id, hf_flex_packet_length,
+		 * hf_flex_sequence, hf_flex_acknowledge
+		*/
 	};
 
 	static gint *ett[] = {
-		&ett_flex,
+		&ett_flex
 	};
 
-	module_t *flex_module;
-	expert_module_t *expert_flex;
+	proto_flex = proto_register_protocol (
+		"Flex Protocol",
+		"FLEX",
+		"flex"
+		);
 
-	proto_flex = proto_register_protocol("Flex ID", "FLEX", "flex");
 	proto_register_field_array(proto_flex, hf, array_length(hf));
 	proto_register_subtree_array(ett, array_length(ett));
-	expert_flex = expert_register_protocol(proto_flex);
-	expert_register_field_array(expert_flex, ei, array_length(ei));
-
-	range_convert_str(&global_flex_port_range, DEFAULT_FLEX_PORT_RANGE, MAX_TCP_PORT);
-
-	flex_module = prefs_register_protocol(proto_flex, proto_reg_handoff_flex);
-	prefs_register_range_preference(flex_module, "flex.ports", "FLEX TCP Ports",
-									"TCP ports to be decoded as FLEX (default: "
-									DEFAULT_FLEX_PORT_RANGE ")",
-									&global_flex_port_range, MAX_TCP_PORT);
-
-	flex_handle = register_dissector("flex", dissect_flex, proto_flex);
-	flex_tap = register_tap("flex");
 }
 
-void
+void 
 proto_reg_handoff_flex(void)
 {
-	static range_t *flex_tcp_port_range;
-	static gboolean initialized = FALSE;
+	static dissector_handle_t flex_handle;
 
-	if (!initialized)
-	{
-		dissector_handle_t flex_handle;
-		flex_handle = create_dissector_handle(dissect_flex, proto_flex);
-		dissector_add_uint("tcp.port", DEFAULT_FLEX_PORT_RANGE, flex_handle);
-		initialized = TRUE;
-	}
-	else
-	{
-		dissector_delete_uint_range("tcp.port", flex_tcp_port_range, flex_handle);
-		g_free(dns_tcp_port_range);
-	}
-
-	flex_tcp_port_range = range_copy(global_flex_tcp_port_range);
-	dissector_add_uint_range("tcp.port", flex_tcp_port_range, flex_handle);
+	flex_handle = create_dissector_handle(dissect_flex, proto_flex);
+	dissector_add_uint("tcp.port", FLEX_PORT, flex_handle);
+//	dissector_add_uint("ethertype", F
 }

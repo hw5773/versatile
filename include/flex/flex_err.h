81ba6ac3bc0b243b29648131145cbb52c1ed2de1
Hyunwoo Lee <hw5773@gmail.com>
2017-11-14 18:58:21 -0800

    revise error

--- include/flex/flex_err.h
+++ include/flex/flex_err.h
@@ -2,7 +2,7 @@
 #define __FLEX_ERR__
 
 // Error Code List (1XX: NO, 2XX: WRONG)
-#define NODEV               100
+#define NO_DEV               100
 #define WRONG_MESSAGE_TYPE  200
 #define WRONG_HEADER        201
 
